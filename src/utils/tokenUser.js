dd303b6163a6f57149e52ea7267704ac53222c3c
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-10-16 23:00:00 -0300

    atualizando erros

--- src/utils/tokenUser.js
+++ src/utils/tokenUser.js
@@ -4,16 +4,16 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 module.exports = async (req, res, next) => {
-  const token = req.header('Authorization');
-  console.log(token, 'token');
+  const token = req.headers.authorization;
   
   if (!token) return res.status(401).json({ message: 'Token not found' });
   
   try {
+    // console.log(token, 'token');
     const decoded = jwt.verify(token, secret);
-    req.id = decoded.data.id;
-    req.userName = decoded.data.name;
-    req.userEmail = decoded.data.email;
+    req.id = decoded.id;
+    req.userName = decoded.displayName;
+    req.userEmail = decoded.email;
     next();
   } catch (e) {
     console.error(e);
